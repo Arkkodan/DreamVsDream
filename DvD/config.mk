92d10b58820ffad8e807e444ba58661ae5bafe83
Mathew <mathewvq@gmail.com>
2014-02-06 00:40:09 -0500

    builds on os x; removed dylib binaries

--- DvD/config.mk
+++ DvD/config.mk
@@ -69,6 +69,6 @@ else
 endif
 
 # flags
-CXXFLAGS	+= -DVERSION=\"$(VERSION)\" -DGAME -std=c++11
-LDFLAGS		+= -lSDL2 -lpng -lz -lsndfile -std=c++11
+CXXFLAGS	+= -DVERSION=\"$(VERSION)\" -DGAME -std=c++11 -I/opt/local/include
+LDFLAGS		+= -lSDL2 -lpng -lz -lsndfile -std=c++11 -L/opt/local/lib
 
